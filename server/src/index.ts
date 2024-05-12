3c06301f378592837307b3ea479bd7945b4d64e2
Lucas Hubert <hello@lucas-hubert.me>
2024-05-12 08:46:22 +0200

    Add typescript

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,9 +1,12 @@
-const express = require('express');
+import express from 'express';
 const app = express();
 const port = 9000;
 
+const one: number = 1;
+const two: number = 2;
+
 app.get('/', (req, res) => {
-    res.send('Hello World!');
+    res.send(`1 + 2 = ${one + two}`);
 });
 
 app.listen(port);
